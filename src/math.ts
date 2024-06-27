d8fad58d45b0407f3639b1888fec208818032c8a
robustprogram <robustprogram@gmail.com>
2024-06-27 18:59:03 +1000

    Pretty bad LERP

---
+++ src/math.ts
@@ -0,0 +1,3 @@
+export function lerp(start: number, stop: number, initial: number) {
+  return start + (stop - start) * initial;
+}
--- src/renderer.ts
+++ src/renderer.ts
@@ -26,6 +26,7 @@
  * ```
  */
 
+import { lerp } from "./math";
 import "./index.css";
 
 const speedDial = document.getElementById("speed-dial");
@@ -33,6 +34,34 @@ const speedNum = document.getElementById("speed-num");
 const rpmDial = document.getElementById("rpm-dial");
 const rpmNum = document.getElementById("rpm-num");
 
+const RPM_DIAL = {
+  next: 0,
+  prev: 0,
+};
+
+let timeSinceLastUpdate = 0;
+let updated = false;
+
+// Used to perform animations such as lerping the dial number
+function animate(timestamp: number) {
+  if (updated) {
+    timeSinceLastUpdate = timestamp;
+    updated = false;
+  }
+
+  const rpmDialCurrent = lerp(
+    RPM_DIAL.prev,
+    RPM_DIAL.next,
+    (timestamp - timeSinceLastUpdate) / 1000
+  );
+
+  rpmNum.innerText = rpmDialCurrent.toFixed(0).toString();
+
+  window.requestAnimationFrame(animate);
+}
+
+// Fuck it, kick start the animation function right from the start ¯\_(ツ)_/¯
+window.requestAnimationFrame(animate);
 
 async function getTelemetry(isDescriptive: boolean) {
   // Comment and uncomment here to run the test server or real server
@@ -51,18 +80,22 @@ async function getTelemetry(isDescriptive: boolean) {
     responseText.replace(`"Color": 0xFF2020`, '"Color": "0xFF2020"')
   );
 
-  const rpm = parseFloat(parsedData["Ext"][0].Value)/*  * 1.60934 */;
-  const rpmAngle = `${rpm*0.03-30}deg`;
+  const rpm = parseFloat(parsedData["Ext"][0].Value); /*  * 1.60934 */
+  const rpmAngle = `${rpm * 0.03 - 30}deg`;
 
   rpmDial.style.setProperty("--dial-angle", rpmAngle);
-  rpmNum.innerText = rpm.toFixed(0).toString();
+  RPM_DIAL.prev = RPM_DIAL.next;
+  RPM_DIAL.next = rpm;
+  // rpmNum.innerText = rpm.toFixed(0).toString();
 
   // speed depends on uploader-side setting, for our case is already kph
-  const speed = parseFloat(parsedData["Ext"][1].Value)/*  * 1.60934 */;
-  const speedAngle = `${speed*1.286-25.71}deg`;
+  const speed = parseFloat(parsedData["Ext"][1].Value); /*  * 1.60934 */
+  const speedAngle = `${speed * 1.286 - 25.71}deg`;
 
   speedDial.style.setProperty("--dial-angle", speedAngle);
   speedNum.innerText = speed.toFixed(0).toString();
+
+  updated = true;
 }
 
 function startTelemetry() {
@@ -72,7 +105,7 @@ function startTelemetry() {
   getTelemetry(true);
 
   // Ping the telemetry server every 0.5 seconds
-  setInterval(() => getTelemetry(false), 500);
+  setInterval(() => getTelemetry(false), 1000);
 }
 
 document.getElementById("start-telemetry").onclick = startTelemetry;
